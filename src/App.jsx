cfd0afc61ece053d5d615b4ee48b79a6fb8ce424
Vovababiichuk <babihuk888@gmail.com>
2023-08-04 17:20:08 +0300

    created start vite+react+tailwind project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='text xl font-bold text-center'>
+      Hello
+    </div>
+  )
+}
+
+export default App
