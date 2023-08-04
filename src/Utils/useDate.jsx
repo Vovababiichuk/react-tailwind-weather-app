273ee8f1c746268d6f6b1680b26004826dff3827
Vovababiichuk <babihuk888@gmail.com>
2023-08-05 01:41:59 +0300

    added all logis

--- src/Utils/useDate.jsx
+++ src/Utils/useDate.jsx
@@ -1,36 +1,24 @@
+import { useEffect, useState } from "react";
 
 export const useDate = () => {
-  const locale = 'en';
-  const [today, setDate] = useState(new Date());
+    const locale = 'en';
+    const [today, setDate] = useState(new Date())
 
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setDate(new Date());
-    }, 60*1000);
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setDate(new Date())
+        }, 60*1000)
 
-    return () => {
-      clearInterval(timer);
+        return () => {
+            clearInterval(timer)
+        }
+    },[])
+
+    const day = today.toLocaleDateString(locale, {weekday: 'long'})
+    const date = `${day}, ${today.getDate()}, ${today.toLocaleDateString(locale, {month: 'long'})}\n\n`
+    const time = today.toLocaleDateString(locale, { hour: 'numeric', hour12: true, minute: 'numeric' })
+
+    return {
+        date, time
     }
-  }, []);
-
-  const day = today.toLocaleDateString(locale, {
-    weekday: 'long',
-    // day: 'numeric',
-    // month: 'long',
-    // year: 'numeric',
-  })
-
-  const date = `${day}, ${today.getDate()}, ${today.toLocaleDateString(locale, {
-    // day: 'numeric',
-    month: 'long',
-    // year: 'numeric',
-  })}\n\n`;
-
-  const time = today.toLocaleTimeString(locale, {
-    hour: 'numeric',
-    hour12: true,
-    minute: 'numeric',
-  })
-
-  return { date, time };
 }
